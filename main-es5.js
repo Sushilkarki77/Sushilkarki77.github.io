5a8f140c61e484273272c1595d737f64e69bc3dc
sushilkarki77 <sushilkarki352@gmail.com>
2020-03-27 22:54:24 +0545

    sfsafs

--- main-es5.js
+++ main-es5.js
@@ -2542,9 +2542,9 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
     TopbarComponent.ɵcmp = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineComponent"]({
       type: TopbarComponent,
       selectors: [["app-topbar"]],
-      decls: 18,
+      decls: 16,
       vars: 0,
-      consts: [[1, "text-center", "bg-bluelight", "covid-alert", "d-flex"], ["href", "https://www.who.int/emergencies/diseases/novel-coronavirus-2019", "target", "blank", 1, "small", "mx-auto"], [1, "d-none", "d-sm-none", "d-md-block", "social-link"], ["href", "https://www.facebook.com/", "target", "_blank"], ["xmlns", "http://www.w3.org/2000/svg", "width", "6.574", "height", "14.166", "viewBox", "0 0 6.574 14.166"], ["id", "Path_675", "data-name", "Path 675", "d", "M201.008,217.719h-2.939v-7.07h-1.035c-.363,0-.363,0-.363-.366v-2.14h1.4c0-.077,0-.139,0-.2.008-.6-.017-1.2.032-1.8a2.577,2.577,0,0,1,2.439-2.548c.874-.07,1.758-.026,2.637-.033,0,0,.009.005.024.015v2.414h-.167c-.454,0-.907,0-1.361,0a.567.567,0,0,0-.649.572c-.028.517-.007,1.036-.007,1.57h2.228c-.013.159-.023.3-.037.443-.065.651-.134,1.3-.2,1.953-.011.114-.072.124-.163.123-.545,0-1.091,0-1.636,0h-.205Z", "transform", "translate(-196.672 -203.553)", "fill", "#fff"], ["href", "https://twitter.com", "target", "_blank"], ["xmlns", "http://www.w3.org/2000/svg", "width", "14.655", "height", "11.726", "viewBox", "0 0 14.655 11.726"], ["id", "Path_676", "data-name", "Path 676", "d", "M111.4,279.778h-1.531c-.418-.072-.84-.129-1.254-.221A8.786,8.786,0,0,1,106,278.481v-.038a5.944,5.944,0,0,0,4.346-1.18,3.251,3.251,0,0,1-1.662-.628,2.856,2.856,0,0,1-1.036-1.435,4.335,4.335,0,0,0,1.2-.076,3.221,3.221,0,0,1-1.622-1.072,2.953,2.953,0,0,1-.644-1.864,2.924,2.924,0,0,0,1.234.317,3.135,3.135,0,0,1-1.136-1.784,2.736,2.736,0,0,1,.373-2.12c.066.049.091.129.143.181a13.987,13.987,0,0,0,1.014.98,8.387,8.387,0,0,0,4.695,1.9c.282.023.289.024.254-.261a2.867,2.867,0,0,1,.837-2.457,3.056,3.056,0,0,1,4.223-.081.294.294,0,0,0,.327.084l1.485-.548c.055-.021.108-.048.162-.073l.036.042a3.111,3.111,0,0,1-1.155,1.5,5.277,5.277,0,0,0,1.585-.424v.076a.576.576,0,0,0-.084.076,6.625,6.625,0,0,1-1.292,1.316.4.4,0,0,0-.108.263c-.023.322-.01.648-.043.969a8.513,8.513,0,0,1-2.237,4.991,8.077,8.077,0,0,1-5.006,2.575C111.721,279.729,111.558,279.754,111.4,279.778Z", "transform", "translate(-106 -268.052)", "fill", "#fff"], [1, "navbar", "navbar-expand-lg", "navbar-dark", "bg-dark", "fixed-top"], ["href", "#"], [1, "font-weight-bold", "navbar-brand"], [1, "mr-2", "head-color"], [1, "head-color"], [1, "navbar-nav"]],
+      consts: [[1, "text-center", "bg-bluelight", "covid-alert", "d-flex"], ["href", "https://www.who.int/emergencies/diseases/novel-coronavirus-2019", "target", "blank", 1, "small", "mx-auto"], [1, "d-none", "d-sm-none", "d-md-block", "social-link"], ["href", "https://www.facebook.com/", "target", "_blank"], ["xmlns", "http://www.w3.org/2000/svg", "width", "6.574", "height", "14.166", "viewBox", "0 0 6.574 14.166"], ["id", "Path_675", "data-name", "Path 675", "d", "M201.008,217.719h-2.939v-7.07h-1.035c-.363,0-.363,0-.363-.366v-2.14h1.4c0-.077,0-.139,0-.2.008-.6-.017-1.2.032-1.8a2.577,2.577,0,0,1,2.439-2.548c.874-.07,1.758-.026,2.637-.033,0,0,.009.005.024.015v2.414h-.167c-.454,0-.907,0-1.361,0a.567.567,0,0,0-.649.572c-.028.517-.007,1.036-.007,1.57h2.228c-.013.159-.023.3-.037.443-.065.651-.134,1.3-.2,1.953-.011.114-.072.124-.163.123-.545,0-1.091,0-1.636,0h-.205Z", "transform", "translate(-196.672 -203.553)", "fill", "#fff"], ["href", "https://twitter.com", "target", "_blank"], ["xmlns", "http://www.w3.org/2000/svg", "width", "14.655", "height", "11.726", "viewBox", "0 0 14.655 11.726"], ["id", "Path_676", "data-name", "Path 676", "d", "M111.4,279.778h-1.531c-.418-.072-.84-.129-1.254-.221A8.786,8.786,0,0,1,106,278.481v-.038a5.944,5.944,0,0,0,4.346-1.18,3.251,3.251,0,0,1-1.662-.628,2.856,2.856,0,0,1-1.036-1.435,4.335,4.335,0,0,0,1.2-.076,3.221,3.221,0,0,1-1.622-1.072,2.953,2.953,0,0,1-.644-1.864,2.924,2.924,0,0,0,1.234.317,3.135,3.135,0,0,1-1.136-1.784,2.736,2.736,0,0,1,.373-2.12c.066.049.091.129.143.181a13.987,13.987,0,0,0,1.014.98,8.387,8.387,0,0,0,4.695,1.9c.282.023.289.024.254-.261a2.867,2.867,0,0,1,.837-2.457,3.056,3.056,0,0,1,4.223-.081.294.294,0,0,0,.327.084l1.485-.548c.055-.021.108-.048.162-.073l.036.042a3.111,3.111,0,0,1-1.155,1.5,5.277,5.277,0,0,0,1.585-.424v.076a.576.576,0,0,0-.084.076,6.625,6.625,0,0,1-1.292,1.316.4.4,0,0,0-.108.263c-.023.322-.01.648-.043.969a8.513,8.513,0,0,1-2.237,4.991,8.077,8.077,0,0,1-5.006,2.575C111.721,279.729,111.558,279.754,111.4,279.778Z", "transform", "translate(-106 -268.052)", "fill", "#fff"], [1, "navbar", "navbar-expand-lg", "navbar-dark", "bg-dark", "fixed-top"], ["href", "#"], [1, "font-weight-bold", "navbar-brand"], [1, "mr-2", "head-color"], [1, "navbar-nav"]],
       template: function TopbarComponent_Template(rf, ctx) {
         if (rf & 1) {
           _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](0, "div", 0);
@@ -2601,17 +2601,11 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
           _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
 
-          _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](15, "span", 13);
-
-          _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](16, "Sushil Karki");
-
           _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
 
           _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
 
-          _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
-
-          _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelement"](17, "div", 14);
+          _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelement"](15, "div", 13);
 
           _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
         }
